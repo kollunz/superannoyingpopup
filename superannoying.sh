57a1cc743c8c4414fb272d292b570786bf1ec183
kollunz <102628492+kollunz@users.noreply.github.com>
2022-07-14 14:55:27 -0700

    Update superannoying.sh

--- superannoying.sh
+++ superannoying.sh
@@ -23,8 +23,8 @@ echo "Annoying $currentUser to update"
 
 /Library/Application\ Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper -windowType fs -title "VIZ IT" -heading "Hi $currentUser! " -description "VIZ IT is requiring you to update your Mac.
 
-Please visit Self Service and run *Install macOS Monterey (macOS 12)*
-This message will stop appearing shortly after upgrading.
+Please visit Self Service to find and run *Install macOS Monterey (macOS 12)*
+This message will stop appearing after you have performed update.
 If you have any questions or concerns, please email help@viz.com. 
 
 This message will disappear shortly." -icon "/System/Library/PreferencePanes/SoftwareUpdate.prefPane/Contents/Resources/SoftwareUpdate.icns" & sleep 10
